2e32ee26ab1c2d2012554e02f1359192e5621964
Arvinbuyan Otgonjargal <Arvinbuyan.O@tengerinsurance.mn>
2024-09-14 14:14:54 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let x = "test";
+// const num = 75;
+
+/*
+const
+ num = 
+ 75;
+*/
+
+let sum = "1" + 1;
+console.log(sum);
---
+++ script2.js
@@ -0,0 +1,27 @@
+// console.log("My first javascript console");
+// alert("Hello world");
+
+let boolean = "";
+let number = 35;
+
+let count = "30" - 20;
+
+let title = "words"; //string
+let num = 75; //number
+let test = false; //boolean
+let x;
+
+let y = null;
+
+let name = "John";
+const age = 30;
+
+let welcome = `Hello, ${name}!`;
+
+//Javascript lesson begin
+
+/*
+Javascript
+lesson
+begin
+*/
